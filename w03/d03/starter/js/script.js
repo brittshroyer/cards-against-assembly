978d85d7b750b1efaa258d0d12a32257c645776a
brittshroyer <britt.shroyer@gmail.com>
2016-11-30 15:54:23 -0600

    random + trending

--- w03/d03/starter/js/script.js
+++ w03/d03/starter/js/script.js
@@ -1,3 +1,5 @@
+
+// http://api.giphy.com/v1/gifs/random
 console.log('script.js loaded');
 
 $(function() {
@@ -6,5 +8,91 @@ $(function() {
 
   // Start with pseudocode!
 
+  var url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC';
+  // var key = 'dc6zaTOxFJmzC&tag';
+
+
+  $('#refresh').on('click', function(e){
+
+    $('#random').empty();
+    var request = $.get(url,function(response){
+      // console.log(request);
+
+      var x = request.responseJSON;
+      var y = x.data;
+      // console.log(y);
+
+      var source = y.image_url;
+
+
+
+
+
+
+
+    var elements = '<img src='+source+'>';
+
+    $('#random').append(elements);
+
+
+  });
+
+
+
+
+
+  });
+
+  $('#trending-results').ready(function(e){
+    var url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+    var request = $.get(url, function(response){
+      // console.log(request);
+      var objectList = request.responseJSON.data;
+
+      console.log(objectList);
+
+
+
+
+
+      for(var i = 0; i<objectList.length; i++){
+        var pic = objectList[i];
+        var img_object = pic.images.downsized;
+        var img_url = pic.images.downsized.url;
+        var img_placeholder = '<img src=' + img_url + '>';
+
+
+
+
+        $('#trending-results').append(img_placeholder);
+
+        }
+
+        
+
+
+
+
+
+    });
+
+
+
+
+
+  });
 
 });
+
+
+  // function updatePic(){
+  //   setInterval(function(){
+  //     for(var i = 0; i<objectList.length; i++){
+  //       var pic = objectList[i];
+  //       var img_object = pic.images.downsized;
+  //       var img_url = pic.images.downsized.url;
+  //       var img_placeholder = '<img src=' + img_url + '>';
+  //       $('#trending-results').append(img_placeholder);
+  //     }
+  //   }, 2000);
+  // }
