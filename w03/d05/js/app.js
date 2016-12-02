a0207859e49093639444e253b6175004754b5f15
brittshroyer <britt.shroyer@gmail.com>
2016-12-02 12:18:47 -0600

    functioning but ugly

--- w03/d05/js/app.js
+++ w03/d05/js/app.js
@@ -1,1 +1,34 @@
 console.log('app.js loaded');
+
+
+$(function(){
+
+  getContent();
+
+
+  $('a').on('click', getContent);
+
+  function getContent(){
+    this.text = this.text || "";
+    var url = 'https://www.reddit.com/'+this.text+'.json';
+      $('#ul').empty();
+      $.get(url, function(response){
+
+        var titleObject = response.data.children;
+        for(var i =0; i<titleObject.length; i++){
+          var titleUrl = titleObject[i].data.title;
+          var imgUrl = titleObject[i].data.thumbnail;
+          var link = titleObject[i].data.url;
+          var score = titleObject[i].data.score;
+          var fullList = ['<li>','<h6>' + score + '</h6>', '<h3>', '<a href="'+link +'">'  + titleUrl + '</a>','</h3>','<img src='+imgUrl+'>','</li>'].join('');
+          $('#ul').append(fullList);
+          console.log(titleObject);
+        }
+
+        
+
+    })
+  }
+
+
+});
