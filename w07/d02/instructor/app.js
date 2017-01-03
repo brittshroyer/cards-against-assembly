edc83e0fb07b21f639ef92870a203af6e3aa5463
Mike Dang <mike.dang@ga.co>
2017-01-03 08:50:57 -0600

    Cleaned up code

--- w07/d02/instructor/app.js
+++ w07/d02/instructor/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({silent: true});
+require('dotenv').config({ silent: true });
 
 var express = require('express');
 var path = require('path');
--- w07/d02/instructor/public/javascripts/app.js
+++ w07/d02/instructor/public/javascripts/app.js
@@ -1,16 +1,3 @@
 (function() {
-  var app = angular.module('CardsAgainstAssembly', ['ngRoute']);
 
-  app.config(function($routeProvider) {
-    $routeProvider
-      .when('/new', {
-        templateUrl: '/templates/_newView.html'
-      })
-      .when('/about', {
-        templateUrl: '/templates/_aboutView.html'
-      })
-      .otherwise({
-        templateUrl: '/templates/_homeView.html'
-      });
-  });
 })();
