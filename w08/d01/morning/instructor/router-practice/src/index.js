13cf78b37c1f64d4b6dffab118ea9e39cd4a6554
Britney Jo <me@britneyjo.com>
2017-01-09 11:31:41 -0600

    add not found route

--- w08/d01/morning/instructor/router-practice/src/index.js
+++ w08/d01/morning/instructor/router-practice/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory, browserHistory} from 'react-router';
 
 import App from './App';
+import NotFound from './NotFound';
 // import './index.css';
 
 ReactDOM.render(
   (<Router history={hashHistory}>
       <Route path="/" component={App}/>
+      <Route path="*" component={NotFound}/>
   </Router>),
   document.getElementById('root')
 );
