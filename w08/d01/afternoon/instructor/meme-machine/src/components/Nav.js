0fb7fe04e498220498cdb118019bddc5630e46aa
Mike Dang <mike.dang@ga.co>
2017-01-09 14:03:09 -0600

    Adding starter code

---
+++ w08/d01/afternoon/instructor/meme-machine/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import NavLink from './NavLink';
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <li><NavLink to="/" onlyActiveOnIndex>Memes</NavLink></li>
+        <li><NavLink to="/add">Add Meme</NavLink></li>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
