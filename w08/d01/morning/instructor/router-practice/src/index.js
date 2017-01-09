d1b8769db3b7f66a9a0cf8b84bcbad836de70d46
Britney Jo <me@britneyjo.com>
2017-01-09 11:07:42 -0600

    create react app and import react router

---
+++ w08/d01/morning/instructor/router-practice/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, browserHistory} from 'react-router';
+
+import App from './App';
+// import './index.css';
+
+ReactDOM.render(
+  (<Router history={hashHistory}>
+      <Route path="/" component={App}/>
+  </Router>),
+  document.getElementById('root')
+);
