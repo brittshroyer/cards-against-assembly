0abf303face2346871fd2b238dbd27ba6be0dce5
brittshroyer <britt.shroyer@gmail.com>
2017-01-11 10:32:31 -0600

    angular practice

--- w07/d02/instructor/public/javascripts/services/card-service.js
+++ w07/d02/instructor/public/javascripts/services/card-service.js
@@ -1,44 +1,24 @@
-(function() {
+(function(){
   var app = angular.module('CardsAgainstAssembly');
-
-  app.service('CardService', function($http) {
-
-    this.getCards = function(callback) {
-
-      $http({
+  app.service('CardService', function($http){
+    this.getCards = function(){
+      http({
         method: 'GET',
         url: '/cards'
-      }).then(function successCallback(response) {
-        console.log('response.data', response.data);
+      }).then(function successCallBack(response){
+        console.log('response', response);
+      }).then(function errorCallback(){
+        console.log('error', error);
 
-        callback(response.data);
-      }, function errorCallback(error) {
-        console.log('Error', error);
       });
 
       // return [
-      //  { question: 'I couldn\'t complete my assignment because ________' },
-      //  { question: 'I get by with a little help from ________' },
-      //  { question: 'The field trip was completely ruined by ________' },
-      //  { question: 'The color of Kyle\'s energy is ________' },
-      //  { question: 'What is my secret power?' }
+      //   { question: 'I couldn\'t complete my assignment because ________' },
+      //   { question: 'I get by with a little help from ________' },
+      //   { question: 'The field trip was completely ruined by ________' },
+      //   { question: 'Kyle is a ________' },
+      //   { question: 'What is my secret power?' }
       // ];
-    }
-
-    this.save = function(question, callback) {
-      $http({
-        method: 'POST',
-        url: '/cards',
-        data: {
-          question: question
-        }
-      }).then(function successCallback(response) {
-        console.log('response.data', response.data);
-
-        callback(response.data);
-      }, function errorCallback(error) {
-        console.log('Error', error);
-      });
     }
   });
 })();
